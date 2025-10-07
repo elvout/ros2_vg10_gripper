e251c1ba329af24ebb7ab45e448423d89832bff9
Elvin Yang <eyy@umich.edu>
2025-10-07 14:19:49 -0400

    Add wip files

---
+++ ros2_vg10_gripper/vg10_driver.py
@@ -0,0 +1,148 @@
+# A significant amount of this file is originally from
+# https://github.com/RyanPaulMcKenna/onRobot/blob/9961464d/onRobot/onRobot/gripper.py
+# under the MIT License.
+
+from io import BytesIO
+from typing import Literal
+
+import pycurl
+
+
+class VG10:
+    def __init__(self, robot_ip: str, vg_id: int) -> None:
+        self.robot_ip = robot_ip
+        self.vg_id = vg_id
+
+    def vg10_release(self, channelA: Literal[0, 1], channelB: Literal[0, 1]) -> None:
+        xml_request = f"""<?xml version="1.0"?>
+        <methodCall>
+        <methodName>vg10_release</methodName>
+            <params>
+                <param>
+                <value><int>{self.vg_id}</int></value>
+                </param>
+                <param>
+                <value><boolean>{channelA}</boolean></value>
+                </param>
+                <param>
+                <value><boolean>{channelB}</boolean></value>
+                </param>
+            </params>
+        </methodCall>"""
+
+        headers = ["Content-Type: application/x-www-form-urlencoded"]
+
+        # headers = ["User-Agent: Python-PycURL", "Accept: application/json"]
+        data = xml_request.replace("\r\n", "").encode()
+        # Create a new cURL object
+        curl = pycurl.Curl()
+
+        # Set the URL to fetch
+        curl.setopt(curl.URL, f"http://{self.robot_ip}:41414")
+        curl.setopt(curl.HTTPHEADER, headers)
+        curl.setopt(curl.POSTFIELDS, data)
+        # Create a BytesIO object to store the response
+        buffer = BytesIO()
+        curl.setopt(curl.WRITEDATA, buffer)
+
+        # Perform the request
+        curl.perform()
+
+        # Get the response body
+        response = buffer.getvalue()
+
+        # Print the response
+        print(response.decode("utf-8"))
+
+        # Close the cURL object
+        curl.close()
+
+    def vg10_grip(self, channel: int, vacuum_percent: float) -> None:
+        """
+        Args:
+            channel: 0 (A), 1 (B), 2 (A and B)
+            vacuum_percent: softgrip => 30 = 30%, firm grip => 60 = 60%
+        """
+
+        xml_request = f"""<?xml version="1.0"?>
+        <methodCall>
+        <methodName>vg10_grip</methodName>
+            <params>
+                <param>
+                    <value><int>{self.vg_id}</int></value>
+                </param>
+                <param>
+                    <value><int>{channel}</int></value>
+                </param>
+                <param>
+                    <value><double>{vacuum_percent}</double></value>
+                </param>
+            </params>
+        </methodCall>
+        """
+
+        headers = ["Content-Type: application/x-www-form-urlencoded"]
+
+        # headers = ["User-Agent: Python-PycURL", "Accept: application/json"]
+        data = xml_request.replace("\r\n", "").encode()
+        # Create a new cURL object
+        curl = pycurl.Curl()
+
+        # Set the URL to fetch
+        curl.setopt(curl.URL, f"http://{self.robot_ip}:41414")
+        curl.setopt(curl.HTTPHEADER, headers)
+        curl.setopt(curl.POSTFIELDS, data)
+        # Create a BytesIO object to store the response
+        buffer = BytesIO()
+        curl.setopt(curl.WRITEDATA, buffer)
+
+        # Perform the request
+        curl.perform()
+
+        # Get the response body
+        response = buffer.getvalue()
+
+        # Print the response
+        print(response.decode("utf-8"))
+
+        # Close the cURL object
+        curl.close()
+
+    def vg10_get_vacuum(self) -> None:
+
+        xml_request = f"""<?xml version="1.0"?>
+        <methodCall>
+        <methodName>vg10_get_vacuum</methodName>
+            <params>
+                <param>
+                    <value><int>{self.vg_id}</int></value>
+                </param>
+        </methodCall>
+        """
+
+        headers = ["Content-Type: application/x-www-form-urlencoded"]
+
+        # headers = ["User-Agent: Python-PycURL", "Accept: application/json"]
+        data = xml_request.replace("\r\n", "").encode()
+        # Create a new cURL object
+        curl = pycurl.Curl()
+
+        # Set the URL to fetch
+        curl.setopt(curl.URL, f"http://{self.robot_ip}:41414")
+        curl.setopt(curl.HTTPHEADER, headers)
+        curl.setopt(curl.POSTFIELDS, data)
+        # Create a BytesIO object to store the response
+        buffer = BytesIO()
+        curl.setopt(curl.WRITEDATA, buffer)
+
+        # Perform the request
+        curl.perform()
+
+        # Get the response body
+        response = buffer.getvalue()
+
+        # Print the response
+        print(response.decode("utf-8"))
+
+        # Close the cURL object
+        curl.close()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = "ros2_vg10_gripper"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=[package_name],
+    data_files=[
+        # Register package with the ament index
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        # Install package.xml for tooling
+        ("share/" + package_name, ["package.xml"]),
+        # (optional) launch files, params, etc.
+        # ('share/' + package_name + '/launch', ['launch/demo.launch.py']),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Elvin Yang",
+    maintainer_email="eyy@umich.edu",
+    description="Basic ROS2 Support for the OnRobot VG10 Gripper with a UR Robot",
+    license="BSD-3-Clause",
+    entry_points={
+        "console_scripts": [
+            "vg10_node = ros2_vg10_gripper.vg10_node:main",
+        ],
+    },
+)
